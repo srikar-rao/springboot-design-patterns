80bcd7a5e2503abb160f29d18fd96d104d5ee719
srikar-rao <67662872+srikar-rao@users.noreply.github.com>
2025-11-16 20:47:46 -0500

    master commit

---
+++ src/test/java/com/dev/org/SpringbootDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
